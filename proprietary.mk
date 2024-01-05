a9ae299848f7667d7d2c5e6f919dc57167065f6e
Chris <codewalker@hardkernel.com>
2024-01-05 14:21:12 +0900

    ODROID-M1/M1S: Add idc for Vu7A+.
    Change-Id: I12bc9f1f18602c9b995a2c38b00c5947ac432e7f

---
+++ proprietary.mk
@@ -0,0 +1,5 @@
+# Input device calibration files
+# for USB HID MULTITOUCH
+PRODUCT_COPY_FILES += \
+        device/hardkernel/proprietary/bin/Vendor_16b4_Product_0704.idc:vendor/usr/idc/Vendor_16b4_Product_0704.idc \
+        device/hardkernel/proprietary/bin/Vendor_16b4_Product_0705.idc:vendor/usr/idc/Vendor_16b4_Product_0705.idc
